b8099f1ae3b739f7534b02be788c6999ec480dac
José Oliveira <jose@habitushealth.net>
2023-04-26 10:58:13 +0100

    token time window

--- src/servers/radius.js
+++ src/servers/radius.js
@@ -94,7 +94,7 @@ class RadiusServer {
                 }
                 const secret = objectHash.sha1(json)
                 const encoded = base32.stringify(Buffer.from(secret));
-                authenticator.options = { ...authenticator.allOptions(), window: 2 * 60 * 24 }
+                authenticator.options = { ...authenticator.allOptions(), window: [2 * 60 * 24,3] }
                 const validOtp = authenticator.check(otp, encoded)
                 if (validOtp) {
                   resolve()
